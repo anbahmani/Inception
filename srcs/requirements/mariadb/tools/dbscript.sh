98f2681d597cce4d2533922a18e5b55a449a64ff
anbahmani <anisbahmani@hotmail.fr>
2023-07-17 20:12:18 +0200

    have to fix mariadb

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,4 +1,5 @@
 service mysql start;
+sleep 1
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
